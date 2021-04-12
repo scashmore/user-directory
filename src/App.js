0168bb57ce5cbfd407bc5f0714fa946b52e5e18d
Susan Ashmore <scashmorecitcom@gmail.com>
2021-04-12 11:03:55 -0400

    working on users

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Header from './components/Header.js';
+import Users from './components/Users.js'
 function App() {
   return (
-  <Header />
-  )}
+    <>
+      <Header />
+      <Users />
+    </>
+  )
+}
 
 export default App;
--- src/components/Users.js
+++ src/components/Users.js
@@ -2,11 +2,32 @@ import React from "react";
 import API from "../utils/Api.js";
 import "./styles.css";
 
-class User extends React.Component {
+class Users extends React.Component {
     state = {
         users: [],
         search: "",
         sort: "",
         col: ""
     };
-};
+
+    componentDidMount() {
+        API.List()
+            .then(res => {
+                const userArr = res.data.results.map(user => {
+                    return {
+                        first: user.name.first,
+                        last: user.name.last,
+                        email: user.email,
+                        dob: user.dob.date,
+                        image: isSecureContext.picture.medium
+                    };
+                });
+                this.setState({ users: userArr });
+            })
+            .catch(err => console.log(err));
+    }
+    handleSearchChange = s => {
+        this.setState({ search: s.target.value});
+    };
+
+}
